f131f93f0be243ab8bf0ba02f8cdbc4eedb28a17
Derek Chan <derekchancts@yahoo.com.hk>
2023-03-19 20:28:53 +0800

    first commit

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ msg: 'Home' })
+  res.json({ msg: 'Home', status: 200 })
 })
 
 app.get('/hello', (req, res) => {
-  res.json({ msg: 'Hello' })
+  res.json({ msg: 'Hello', status: 200 })
 })
 
 app.get('/dashboard', (req, res) => {
-  res.json({ msg: 'dashboard' })
+  res.json({ msg: 'dashboard', status: 200 })
 })
 
 app.listen(5000, () => console.log('server runnign on port 5000'))
